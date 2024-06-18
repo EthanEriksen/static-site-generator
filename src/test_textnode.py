814d30f44bd785d89d5d74c42e6b6b884ca463f7
EthanEriksen <13228370+EthanEriksen@users.noreply.github.com>
2024-06-18 18:57:29 -0400

    write unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a different text node", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_text_type(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italics")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", "bold", "/home")
+        node2 = TextNode("This is a text node", "bold" "/info")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
