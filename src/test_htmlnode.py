da5d41e90a4ca00f222cf55f3413c33030f29a7c
EthanEriksen <13228370+EthanEriksen@users.noreply.github.com>
2024-06-20 12:08:25 -0400

    expand HTMLNode test

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,7 +4,9 @@ from htmlnode import HTMLNode
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("p", "Lorem ipsum", None, {"class": "body-text"})
+        node2 = HTMLNode("a", "Link text", None, {"href": "https://boot.dev/"})
         self.assertEqual(node.props_to_html(), " class=\"body-text\"")
+        self.assertEqual(node2.props_to_html(), " href=\"https://boot.dev/\"")
 
 if __name__ == "__main__":
     unittest.main()
