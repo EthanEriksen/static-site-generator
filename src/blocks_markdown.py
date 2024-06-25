395fd18e71ea3deb5bd33f4c6a173d44c5af17e0
EthanEriksen <13228370+EthanEriksen@users.noreply.github.com>
2024-06-24 21:29:48 -0400

    markdown block type variables

--- src/blocks_markdown.py
+++ src/blocks_markdown.py
@@ -1,3 +1,10 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
     raw_blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -10,3 +17,6 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(raw_block)
 
     return filtered_blocks
+
+
+def block_to_block_type(block):
